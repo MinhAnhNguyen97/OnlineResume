2d48250c6f4cf2180c64bc761d9a9aac7f2bb5d8
Nguyen <nguyenam@cua.edu>
2025-03-06 11:47:53 -0500

    changes

---
+++ data/contents.js
@@ -0,0 +1,51 @@
+const mainContent = {
+    pageName: [ 
+        {
+            id: "page-name",
+           title: "Page-name",
+           articles: "MINH ANH NGUYEN's WEBSITE",
+        }
+    ],
+    mainSection1: [
+        {
+            id: "main-section-1_title",
+            title: "main-section-1-title",
+            articles: "INSPIRATION",
+            imgSrc: "images/photo-1518837695005-2083093ee35b.jpeg",
+            textOnImg: "In the midst of winter, I found there was, within me, an invicible summer.",
+        }
+    ],
+    mainSection2: [
+        {
+            id: "main-section-2-title",
+            title: "main-section-2-title",
+            articles: "DATA ANALYTICS / MACHINE LEARNING PROJECTS",
+                title: "section-2-first-flexbox",
+                articles: "Machine Learning Projects",
+                imgSrc: "images/Untitled-2.png",
+                title: "section-2-second-flexbox",
+                articles: "Tableau Projects",
+                imgSrc: "images/Untitled-1.png",
+                title: "section-2-third-flexbox",
+                articles: "Statistical Analysis Projects",
+                imgSrc: "images/Untitled.png",
+        }
+    ],
+    mainSection3: [
+        {
+             id: "main-section-3-title",
+             title: "main-section-3-title",
+             articles: "ART",
+                 title: "section-3-first-flexbox",
+                 articles: "Flowers in a flower shop - Saigon",
+                 imgSrc: "images/IMG_1028.jpeg",
+                 title: "section-3-second-flexbox",
+                 articles: "Architecture - Vermont",
+                 imgSrc: "images/IMG_1338.JPEG",
+                 title: "section-3-third-flexbox",
+                 articles: "White Blooms - New York City",
+                 imgSrc: "images/IMG_0116.jpeg",
+        } 
+     ]
+}   
+
